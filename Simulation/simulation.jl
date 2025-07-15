ed8670593e09ed265f61668b9e047d9966b4ac24
Hauke <h.uhde@tu-braunschweig.de>
2025-07-15 16:19:34 +0200

    now training a model quick for testing convergance

--- Simulation/simulation.jl
+++ Simulation/simulation.jl
@@ -2,63 +2,112 @@ import Makie
 import LaTeXStrings
 import CairoMakie
 import DelimitedFiles
+import Flux
+import LinearAlgebra
 
 
-function create_batch_signals_full_data(batch_size::Int, listening_length::Int; mic_rate::Int=44000, dt::Float64=1/mic_rate)
-    rand_float_0_1 = rand(Float64,3*batch_size)
-    results = zeros(batch_size,listening_length)
-    for index in range(0,batch_size-1)
-        results[index+1,:] = rand_float_0_1[3*index+1]*sin.(rand_float_0_1[3*index+2]* dt *(0:listening_length-1) .+ rand_float_0_1[3*index+3])
+function create_batch_signals_full_data(batch_size_create_data::Int, listening_length::Int; mic_rate::Int=44000, dt::Float64=1/mic_rate)
+    rand_float_0_1 = rand(Float64,3*batch_size_create_data)
+    results = zeros(batch_size_create_data,listening_length)
+    for index in range(0,batch_size_create_data-1)
+        results[index+1,:] = rand_float_0_1[3*index+1]*sin.(rand_float_0_1[3*index+2]*20_000* dt *(0:listening_length-1) .+ rand_float_0_1[3*index+3])
     end
     return results
 end
 
 function save_signals_full_data(saving_data_path::String,signals)
-    open(saving_data_path*"train_batch.txt","w") do file
+    open(saving_data_path,"w") do file
         write.(file,join.(eachrow(signals),",").*"\n")
     end
 end
 
+function wrapper_create_save_data_full_data(batch_size_create_data::Int,listening_length::Int,saving_data_path::String)
+    save_signals_full_data(saving_data_path,create_batch_signals_full_data(batch_size_create_data,listening_length))
+end
 
-function wrapper_create_save_data_full_data(batch_size::Int,listening_length::Int,saving_data_path::String)
-    save_signals_full_data(saving_data_path,create_batch_signals_full_data(batch_size,listening_length))
+function prepare_data_full_learn(data;ear_positions=[[1,0,0],[0,1,0],[0,0,1]],speed_sound = 343., mic_rate::Int=44000 , dt::Float64=1/mic_rate)
+    listening_length = length(data[1,:])
+    batch_size = length(data[:,1])
+    positions_sound = rand(3*batch_size)*200 .- 100
+
+    distances = [LinearAlgebra.norm.([positions_sound[3*index+1:3*index+3] .- ear_positions[1],positions_sound[3*index+1:3*index+3] .- ear_positions[2],positions_sound[3*index+1:3*index+3] .- ear_positions[3]]) for index in range(0,batch_size-1)]/ speed_sound
+    times = [ceil.(Int,(time .- minimum(time))/dt) for time in distances]
+    
+    results = [[[0. for _ in 1:listening_length] for _ in 1:3] for _ in 1:batch_size]
+    for index in range(1,batch_size)
+        mic_data = [let shifted = (1/distances[index][n_ear]^2)*circshift(data[index,:],times[index][n_ear])
+        shifted[1:times[index][n_ear]] .= 0
+        shifted
+        end for n_ear in 1:3]
+        results[index] = mic_data 
+    end 
+    return results,positions_sound
 end
 
 
-function create_save_batch_signal_encoded(batch_size::Int,saving_data_path::String)
-    signals = rand(Float64,batch_size,3)
-    open(saving_data_path*"train_batch.txt","w") do file
-        write.(file,join.(eachrow(signals),",").*"\n")
+function create_save_batch_signal_encoded(batch_size_create_data::Int,saving_data_path::String)
+    open(saving_data_path,"w") do file
+        write.(file,join.(eachrow(rand(Float64,batch_size_create_data,6)),",").*"\n")
+    end
+    return nothing
+end
+
+struct Data_reader_organiser
+    max_num_reads::Int
+    current_read::Int
+end
+
+function read_encoded_data_to_measurement(saving_data_path::String,learn_batch_size::Int,listening_length::Int, sound_position ;ear_positions=[[1,0,0],[0,1,0],[0,0,1]],speed_sound = 343.)
+    data_to_learn = zeros(learn_batch_size,3*listening_length)
+    open(saving_data_path,"r") do file
+        for index in range(1,learn_batch_size)
+
+        end
     end
 end
 
-function read_encoded_data_to_measurement()
+function wrapper_read_write()
     
 end
 
 saving_plot_path = (@__DIR__)*"/plots/"
 saving_data_path = (@__DIR__)*"/data/"
+saving_data_to = saving_data_path*"train_batch.txt"
 
 println("HI :)")
 
-batch_size = 10000
+batch_size_create_data = 5
 listening_length = 44_00
 
+#@time wrapper_create_save_data_full_data(batch_size_create_data,listening_length,saving_data_to)
 
-#@time wrapper_create_save_data_full_data(batch_size,listening_length,saving_data_path)
+data_learn,positions = prepare_data_full_learn(create_batch_signals_full_data(batch_size_create_data,listening_length))
+#=
+model = Flux.Chain(Flux.Dense(3*listening_length=>500,Flux.relu),
+    Flux.Dense(500=>100,Flux.relu),
+    Flux.Dense(100=>3,Flux.relu))
 
-@time create_save_batch_signal_encoded(batch_size,saving_data_path)
 
-print("done")
+data =create_batch_signals_full_data(batch_size_create_data,listening_length)
+
+opt_state = Flux.setup(Flux.Adam(0.01), model)
+Flux.train!(model, train_set, opt_state)
+=#
+
+#@time create_save_batch_signal_encoded(batch_size_create_data,saving_data_to)
+
+#print("done")
+
 #=
 fig = Makie.Figure()
 ax = Makie.Axis(fig[1, 1],title = "many signals",
 xlabel = LaTeXStrings.LaTeXString("time"),
 ylabel = LaTeXStrings.LaTeXString("signal(t)"))
 for index_ear in range(1,3)
-    Makie.lines!(ax,range(0.,1.,length = length(signals[1,:])), signals[index_ear,:],label="signal_ear: $index_ear")
+    Makie.lines!(ax,range(0.,1.,length = length(data_learn[1][index_ear])), data_learn[1][index_ear],label="signal_ear: $index_ear")
 end
 Makie.axislegend()
 CairoMakie.display(fig)
 CairoMakie.save(saving_plot_path*"Test.png",fig)
 =#
+
