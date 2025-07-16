253ceed1c17d34ba04dab081f62b58ae6ed66e25
Hauke <h.uhde@tu-braunschweig.de>
2025-07-16 16:44:29 +0200

    reformated the output of data generation :)

--- Simulation/simulation.jl
+++ Simulation/simulation.jl
@@ -1,9 +1,9 @@
 import Makie
 import LaTeXStrings
 import CairoMakie
-import DelimitedFiles
 import Flux
 import LinearAlgebra
+import StaticArrays
 
 
 function create_batch_signals_full_data(batch_size_create_data::Int, listening_length::Int; mic_rate::Int=44000, dt::Float64=1/mic_rate)
@@ -39,24 +39,49 @@ function prepare_data_full_learn(data;ear_positions=[[1,0,0],[0,1,0],[0,0,1]],sp
         shifted
         end for n_ear in 1:3]
         results[index] = mic_data 
-    end 
+    end
     return results,positions_sound
 end
 
-function faster_prepare_data_full_learn(data;ear_positions=[[1,0,0],[0,1,0],[0,0,1]],speed_sound = 343., mic_rate::Int=44000 , dt::Float64=1/mic_rate)
+function faster_prepare_data_full_learn(data;ear_positions=StaticArrays.SVector{3,Float64}.((1,0,0),(0,1,0),(0,0,1)),speed_sound = 343., mic_rate::Int=44000 , dt::Float64=1/mic_rate)
     batch_size, listening_length = size(data)
     positions_sound = rand(3*batch_size)*200 .- 100
 
-    @inbounds distances = [[LinearAlgebra.norm(positions_sound[3*index+1:3*index+3] .- ear_positions[n_ear]) for n_ear in 1:3] for index in range(0,batch_size-1)]
+    @inbounds distances = [@StaticArrays.SVector [LinearAlgebra.norm(positions_sound[3*index+1:3*index+3] .- ear_positions[n_ear]) for n_ear in 1:3] for index in range(0,batch_size-1)]
     times = [ceil.(Int,(dist/speed_sound .- minimum(dist/speed_sound))/dt) for dist in distances]
     
-    results = [[[0. for _ in 1:listening_length] for _ in 1:3] for _ in 1:batch_size]
+    results = [[Vector{Float64}(undef, listening_length) for _ in 1:3] for _ in 1:batch_size]
     @inbounds for index in range(1,batch_size)
+        row = @view data[index, :]
         @inbounds for n_ear in 1:3
-            results[index][n_ear] = (1/distances[index][n_ear]^2)*circshift(data[index,:],times[index][n_ear])
-            results[index][n_ear][1:times[index][n_ear]] .= 0
+            circshift!(results[index][n_ear],row,times[index][n_ear])
+            results[index][n_ear] *= (1/distances[index][n_ear]^2)
+            @inbounds results[index][n_ear][1:times[index][n_ear]] .= 0
         end
-    end 
+    end
+    return results,positions_sound
+end
+
+
+function less_d_faster_prepare_data_full_learn(data;ear_positions=StaticArrays.SVector{3,Float64}.((1,0,0),(0,1,0),(0,0,1)),speed_sound = 343., mic_rate::Int=44000 , dt::Float64=1/mic_rate)
+    batch_size, listening_length = size(data)
+    positions_sound = rand(3*batch_size)*200 .- 100
+
+    @inbounds distances = [@StaticArrays.SVector [LinearAlgebra.norm(positions_sound[3*index+1:3*index+3] .- ear_positions[n_ear]) for n_ear in 1:3] for index in range(0,batch_size-1)]
+    times = [ceil.(Int,(dist/speed_sound .- minimum(dist/speed_sound))/dt) for dist in distances]
+    
+    results = [Vector{Float64}(undef, 3*listening_length) for _ in 1:batch_size]
+    for index in range(1,batch_size)    #@inbounds
+        row = @view data[index, :]
+        for n_ear in 1:3    #@inbounds
+            l_index = (n_ear-1)*listening_length+1
+            u_index = n_ear*listening_length
+            res_write_to = @view results[index][l_index:u_index]
+            circshift!(res_write_to,row,times[index][n_ear])
+            results[index][l_index:u_index] *= (1/distances[index][n_ear]^2)
+            results[index][l_index:l_index+times[index][n_ear]] .= 0    #@inbounds
+        end
+    end
     return results,positions_sound
 end
 
@@ -99,7 +124,9 @@ listening_length = 44_00
 
 #@time wrapper_create_save_data_full_data(batch_size_create_data,listening_length,saving_data_to)
 
-@time data_learn_f,positions_f = faster_prepare_data_full_learn(create_batch_signals_full_data(batch_size_create_data,listening_length))
+#@time data_learn,positions = faster_prepare_data_full_learn(create_batch_signals_full_data(batch_size_create_data,listening_length))
+
+@time data_learn,positions = less_d_faster_prepare_data_full_learn(create_batch_signals_full_data(batch_size_create_data,listening_length))
 
 #@time data_learn,positions = prepare_data_full_learn(create_batch_signals_full_data(batch_size_create_data,listening_length))
 
@@ -121,16 +148,16 @@ Flux.train!(model, train_set, opt_state)
 
 #print("done")
 
-#=
+#
 fig = Makie.Figure()
 ax = Makie.Axis(fig[1, 1],title = "many signals",
 xlabel = LaTeXStrings.LaTeXString("time"),
 ylabel = LaTeXStrings.LaTeXString("signal(t)"))
-for index_ear in range(1,3)
-    Makie.lines!(ax,range(0.,1.,length = length(data_learn[1][index_ear])), data_learn[1][index_ear],label="signal_ear: $index_ear")
+for index_ear in range(1,1)
+    Makie.lines!(ax, data_learn[index_ear],label="signal concatinated: $index_ear")
 end
 Makie.axislegend()
 CairoMakie.display(fig)
-CairoMakie.save(saving_plot_path*"Test.png",fig)
-=#
+CairoMakie.save(saving_plot_path*"Test_less_d.png",fig)
+#
 
