4842d0101dd2b87ec9e9a76798f01c5f751c9db8
Hauke <h.uhde@tu-braunschweig.de>
2025-07-30 16:38:36 +0200

    done with concetration but started with classefier

--- Simulation/classifier.jl
+++ Simulation/classifier.jl
@@ -4,31 +4,23 @@ import CairoMakie
 import Flux
 import LinearAlgebra
 import StaticArrays
-import CUDA
+#import CUDA
 import BSON
+import ScikitLearn
 
 
-function only_times_and_dist(batch_size, value_bounds=(0.,200.); ear_positions=StaticArrays.SVector{3,Float64}.((1,0,0,0),(0,1,0,0),(0,0,1,0)), speed_sound = 343., mic_rate::Int=44000, dt::Float64=1/mic_rate, num_ears=length(ear_positions))
+function spherical_only_times_and_dist(batch_size, value_bounds=(0.,100.); ear_positions=StaticArrays.SVector{3,Float64}.((1,0,0,0),(0,1,0,0),(0,0,1,0)), speed_sound = 343., mic_rate::Int=44000, dt::Float64=1/mic_rate, num_ears=length(ear_positions))
     positions_sound = rand(3*batch_size)
     l_bound = value_bounds[1]
     u_bound = value_bounds[2]
     diff = u_bound-l_bound
-    mod_value = 1
     for index in 1:batch_size
-        value = positions_sound[index]
-        if index%(4)==1 #4 weil wir in 3d arbeiten
-            if value<0.5
-                positions_sound[index] = -2*value*diff-l_bound
-            else
-                positions_sound[index] = (2*value-1)*diff+l_bound
-            end
-        else
-            positions_sound[index] = value*2*u_bound-u_bound
-        end
-        if index%9==0
-            mod_value+=1
-            mod_value%=4
-        end
+        radius = sqrt(positions_sound[3*index-2])*diff+l_bound
+        theta = 2*pi*positions_sound[3*index-1]
+        phi = 2*pi*positions_sound[3*index]
+        positions_sound[3*index-2] = radius*sin(theta)*cos(phi)
+        positions_sound[3*index-1] = radius*sin(theta)*sin(phi)
+        positions_sound[3*index] = radius*cos(theta)
     end
     
     results = [Vector{Float64}(undef, 2*num_ears) for _ in 1:batch_size]
@@ -42,13 +34,11 @@ function only_times_and_dist(batch_size, value_bounds=(0.,200.); ear_positions=S
     return results,positions_sound
 end
 
-function only_2d_positions(batch_size, value_bounds=(0.,200.))
+function spherical_only_2d_positions(batch_size, value_bounds=(0.,100.))
     positions = rand(2*batch_size)
     l_bound = value_bounds[1]
     u_bound = value_bounds[2]
     diff = u_bound-l_bound
-    mod_value =1
-    dim_sqr = dimension^2
     for index in 1:batch_size
         radius = sqrt(positions[2*index-1])*diff+l_bound
         theta = 2*pi*positions[2*index]
@@ -58,66 +48,18 @@ function only_2d_positions(batch_size, value_bounds=(0.,200.))
     return positions
 end
 
-function give_model_wild_do_ki_only_times_actual_batches(batch_size_create_data, batches_per_epoch,value_bounds = (0.,200.) ;epochs=2,new_data = 5, print_every = batches_per_epoch//1000, evaluation_batch_size = batch_size_create_data, model = nothing)
-    device = Flux.gpu_device()
-    @time data_learn,positions = only_times_and_dist(batch_size_create_data*batches_per_epoch,value_bounds)|>device
-    data_test,positions_test = only_times_and_dist(evaluation_batch_size,value_bounds)|>device
-
-    model = model|>device
-    opt_state = Flux.setup(Flux.Adam(), model)
-    #opt_state = Flux.setup(Flux.NADAM(), model)
-
-    train_accuracies = zeros(epochs)
-    test_accuracies = zeros(epochs)
-
+function do_classification(batch_size_create_data, batches_per_epoch;epochs=2,new_data = 5, print_every = batches_per_epoch//1000, evaluation_batch_size = batch_size_create_data,)
+    data, positions = spherical_only_times_and_dist(batch_size_create_data*batches_per_epoch,(0.,100.))
+    target = ceil.(Int,[LinearAlgebra.norm(positions[3*index-2:3*index]) for index in 1:batch_size_create_data*batches_per_epoch])
+    clas = ScikitLearn.RandomForestClassifier(50)
     for epoch in 1:epochs
-        if epoch%new_data==0
-            data_learn,positions = only_times_and_dist(batch_size_create_data*batches_per_epoch,value_bounds)|>device
+        target_one_hot = zeros(batch_size_create_data,200)
+        for index in 1:batch_size_create_data
+            target_one_hot[index,target[(epoch-1)*batch_size_create_data+index]]=1
         end
-
-        for index in 1:batches_per_epoch
-            batch_start = (index - 1) * batch_size_create_data + 1
-            batch_end = index * batch_size_create_data
-            raw_batch = data_learn[batch_start:batch_end]
-            data_batch = reduce(hcat, raw_batch)
-            positions_batch = positions[3*(batch_start - 1) + 1 : 3*batch_end]
-            positions_batch = reshape(positions_batch, 3, :)
-            grads = Flux.gradient(model) do m
-                Flux.Losses.mse(m(data_batch), positions_batch)+ 0.5* mapreduce(p -> sum(abs2, p), +, Flux.trainables(m))
-            end
-            Flux.update!(opt_state, model, grads[1])
-
-            if index%print_every==0
-                print("\r")
-                print("Epoch: $epoch Advanced: $(round(index/(batches_per_epoch)*100,digits=1))%")
-            end
-        end
-
-        
-        g_mse_train = 0.
-        g_mse_test = 0.
-        for index in 1:evaluation_batch_size
-            g_mse_train += Flux.Losses.mae(model(data_learn[index]),positions[3*(index-1)+1:3*index])/evaluation_batch_size#sqrt(sum((model(data_learn[index]) .- positions[3*(index-1)+1:3*index]).^2))/evaluation_batch_size
-            g_mse_test += Flux.Losses.mae(model(data_test[index]),positions_test[3*(index-1)+1:3*index])/evaluation_batch_size#sqrt(sum((model(data_test[index]) .- positions_test[3*(index-1)+1:3*index]).^2))/evaluation_batch_size
-        end
-        train_accuracies[epoch] = g_mse_train
-        test_accuracies[epoch] = g_mse_test
-        
-        print("\r")
-        print("Epoch: $epoch , g_mse_train: $g_mse_train , g_mse_test: $g_mse_test")
+        clas.fit(reshape(data,1,length(data)),target_one_hot)
     end
-    
-    g_mse = 0.
-    for index in 1:batch_size_create_data
-        g_mse += Flux.Losses.mae(model(data_learn[index]),positions[3*(index-1)+1:3*index])/batch_size_create_data
-    end
-    print("\r")
-    println()
-    println("the average mse is: $g_mse")
 
-    println("the supposed position is: $(positions[1:3]) the prediction is: $(model(data_learn[1]))")
-
-    return train_accuracies,test_accuracies,model
 end
 
 function plot_accuracy(train_acc,test_acc,plot_name)
@@ -156,45 +98,23 @@ saving_data_path = (@__DIR__)*"/mini_models_data/"
 println("HI :)")
 
 #model_name = "wild_test_model"
-batch_size_create_data = 3_000
+batch_size_create_data = 3
 listening_length = 8
 num_ears = 4
 epochs = 75
 new_data = 2
 eval_b_size = 100
-batches_per_epoch = 10
+batches_per_epoch = 1
 print_new_data = batches_per_epoch//100
 
-@time positions_plot = dimension_only_positions(2,1000,(1.,2.))
-plot_data(positions_plot,"data_model_$model_value","evaluation_area:[abs($(model_value-1)),abs($(model_value))]")
-println("plotted data")
-
 #=
-for model_value in 1:100
-    if model_value<6
-        @time positions_plot = dimension_only_positions(2,1000,(float(model_value-1),float(model_value)))
-        plot_data(positions_plot,"data_model_$model_value","evaluation_area:[abs($(model_value-1)),abs($(model_value))]")
-        println("plotted data")
-    end
-
-    model = BSON.load(loading_data_path*"wild_test_model"*".bson")[:model]
-    model_state = BSON.load(loading_data_path*"wild_test_model"*"_state"*".bson")[:model_state]
-    Flux.loadmodel!(model,model_state)
-    model_name = "wild_test_model_$model_value"
-    println("now training model: $model_value")
-    @time train_acc,test_acc,model = give_model_wild_do_ki_only_times_actual_batches(batch_size_create_data,batches_per_epoch,(float(model_value-1),float(model_value)),epochs=epochs ,new_data=new_data,print_every=print_new_data,evaluation_batch_size=eval_b_size,model=model)
-    cpu_device = Flux.cpu_device()
-    model = model|>cpu_device
-    BSON.@save saving_data_path*model_name*"_state"*".bson" model_state=Flux.state(model)
-    BSON.@save saving_data_path*model_name*".bson" model
-
-    println("best accuracy in test data was: $(minimum(test_acc))")
-    if model_value%10==1
-        plot_accuracy(train_acc,test_acc,"wild_test_$model_value")
-    end
-end
-
+l_bound,u_bound = 2,3
+@time positions_plot = only_2d_positions(10000,(l_bound,u_bound))
+plot_data(positions_plot,"test_spherical","evaluation_area:[abs($(l_bound)),abs($(u_bound))]")
+println("plotted data")
 =#
+
+do_classification(batch_size_create_data,batches_per_epoch)
 
 print("done")
 
